f2ba6550402ecfd0020d5dfdb35298906848f7d2
Amanbay Kenesary <koni9812@gmail.com>
2024-06-09 14:07:00 +0500

    dinner

---
+++ codeWars-12.js
@@ -0,0 +1,15 @@
+//You get an array of numbers, return the sum of all of the positives ones.
+
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+//Note: if there is nothing to sum, the sum is default to 0.
+
+//Solution:
+
+function positiveSum(arr) {
+  arr
+    .filter((number) => number > 0)
+    .reduce((total, currentSum) => {
+      return total + currentSum
+    }, 0)
+}
